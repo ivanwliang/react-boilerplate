ddc62badeed949f653c21367d81d7ded38d2bb9d
Ivan Liang <ivanwenliang@gmail.com>
2019-07-11 17:24:45 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends React {
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "sanitize.css";
+import "sanitize.css/typography.css";
+import "sanitize.css/forms.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
